ac5f8da533da618214e3eb37904ae7d01d31e9e8
Brian Lee <48812505+BrianYLee@users.noreply.github.com>
2024-08-31 18:48:59 +0800

    Update AuthContext.jsx
    manage loader in effect

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,46 +1,22 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import Taro from '@tarojs/taro';
 
-import AuthService from '../services/Auth/AuthService'
+import AuthService from '../services/Auth/AuthService';
 import { useLoader } from './LoaderContext';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-
-    console.log("AuthProvider loaded");
+    const [authLoading, setAuthLoading] = useState(true);
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [isEmployee, setIsEmployee] = useState(false);
     const [userData, setUserData] = useState(null);
 
     const { showLoader, hideLoader } = useLoader();
 
-    const redirectOnAuthChange = () => {
-        if (isAuthenticated && isEmployee) {
-            // goto appointments
-            console.log('redir auth emp')
-            Taro.switchTab({
-                url: '/pages/Appointments/index'
-            });
-        }
-        else if (isAuthenticated && !isEmployee) {
-            // goto registration page
-            console.log('redir auth')
-            Taro.redirectTo({
-                url: '/pages/Registration/index'
-            });
-        }
-        else {
-            console.log('relaunch')
-            Taro.reLaunch({
-                url: '/pages/Welcome/index'
-            });
-        }
-    }
-
     const handleLogin = async () => {
         try {
-            showLoader();
+            setAuthLoading(true);
             const taroRes = await Taro.login();
             if (taroRes.code) {
                 const credentials = taroRes.code;
@@ -60,12 +36,13 @@ export const AuthProvider = ({ children }) => {
                 duration: 2000
             });
         } finally {
-            hideLoader();
+            setAuthLoading(false);
         }
     };
 
     const wechat_login = async (code) => {
         try {
+            setAuthLoading(true);
             const { success, message, openId, isEmployee } = await AuthService.login(code);
             const userData = {
                 openid: openId,
@@ -81,10 +58,14 @@ export const AuthProvider = ({ children }) => {
                 icon: 'fail',
                 duration: 2000
             })
+        } finally {
+            setAuthLoading(false);
         }
-    }
+    };
+
     const wechat_renew = async (openid) => {
         try {
+            setAuthLoading(true);
             const { success, message, openId, isEmployee } = await AuthService.renew(openid);
             const userData = {
                 openid: openId,
@@ -104,6 +85,8 @@ export const AuthProvider = ({ children }) => {
                 icon: 'fail',
                 duration: 2000
             });
+        } finally {
+            setAuthLoading(false);
         }
     }
 
@@ -119,37 +102,26 @@ export const AuthProvider = ({ children }) => {
     };
 
     const checkAuthStatus = async () => {
-        showLoader();
-        console.log('AuthContext: checkAuthStatus: invoked.');
         const userData = Taro.getStorageSync('userInfo');
         if (userData && userData.openid) {
-            console.log('AuthContext: checkAuthStatus: userInfo found with wechat openid: ' + userData.openid);
             try {
                 await wechat_renew(userData.openid);
             } catch (err) {
                 console.log(err);
             } finally {
-                hideLoader();
+                //setAuthLoading(false);
                 //redirectOnAuthChange();
             }
-            /*
-            if (userData.isEmployee) {
-                console.log('user is also an employee');
-                setIsAuthenticated(prev => prev = true);
-                setIsEmployee(prev => prev = true);
-            }
-            else {
-                setIsAuthenticated(prev => prev = true);
-                setIsEmployee(prev => prev = false);
-            }
-            setUserData(prev => prev = userData);
-            */
-        }
-        else {
-            console.log('AuthContext: checkAuthStatus: no user found')
-            hideLoader();
         }
     };
+
+    useEffect(() => {
+        if (authLoading) {
+            showLoader();
+        } else if (!authLoading) {
+            hideLoader();
+        }
+    }, [authLoading]);
 
     useEffect(() => {
         checkAuthStatus();
@@ -166,55 +138,3 @@ export const useAuth = () => {
     return useContext(AuthContext);
 };
 
-/*
-    const handleLogin = async (userProfile) => {
-        showLoader();
-        try {
-            const userInfo = userProfile?.detail?.userInfo;
-            if (!userInfo) {
-                throw new Error('could not get user info');
-            }
-            const taroRes = await Taro.login();
-            if (taroRes.code) {
-                const credentials = taroRes.code;
-                wechat_login(userInfo, credentials);
-            } else {
-                Taro.showToast({
-                    title: 'Login failed, please try again!',
-                    icon: 'error',
-                    duration: 2000
-                });
-            }
-        } catch (error) {
-            console.error('Error during login:', error);
-            Taro.showToast({
-                title: 'An error occurred during login',
-                icon: 'none',
-                duration: 2000
-            });
-        } finally {
-            hideLoader();
-        }
-    };
-
-    const wechat_login = async (userInfo, code) => {
-        try {
-            const { success, message, openId, isEmployee } = await AuthService.login(code);
-            const userData = {
-                ...userInfo,
-                openid: openId,
-                isEmployee: isEmployee
-            }
-            setIsAuthenticated(true);
-            setIsEmployee(isEmployee);
-            setUserData(userData);
-            Taro.setStorageSync('userInfo', userData);
-        } catch (error) {
-            Taro.showToast({
-                title: 'Login failed',
-                icon: 'fail',
-                duration: 2000
-            })
-        }
-    }
-*/
