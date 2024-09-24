b9d7e6d5e27d4e1d5fbea27a266794b346bfb9bc
Brian Lee <48812505+BrianYLee@users.noreply.github.com>
2024-09-24 13:37:55 +0800

    add securityCarf

--- src/components/AppointmentCard/SecurityCard.jsx
+++ src/components/AppointmentCard/SecurityCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ButtonGroup from './ButtonGroup'
 import { View } from '@tarojs/components'
 import { AtCard, AtButton } from 'taro-ui'
@@ -6,7 +6,8 @@ import { AtCard, AtButton } from 'taro-ui'
 // styling
 import './SecurityCard.scss'
 
-const SecurityCard = ({ apmtInfo, today, handleEdit }) => {
+const SecurityCard = ({ apmtInfo, handleCheckIn, handleCheckOut }) => {
+    // checkin functions
     return (
         <View className='appointment-card security-card'>
             <AtCard
@@ -16,8 +17,23 @@ const SecurityCard = ({ apmtInfo, today, handleEdit }) => {
             >
                 {apmtInfo?.vehicles?.map((vehicle, idx) => (
                     <View className={`at-row at-row__align--center ${idx%2 == 0 ? 'even' : 'odd'}`}>
-                        <View className='at-col at-col-9 infoListing'>{vehicle.plate}</View>
-                        <View className='at-col at-col__offset-1 infoListing info'><AtButton circle size='small' type='primary'>B</AtButton></View>
+                        <View className='at-col infoListing'>{vehicle.plate}</View>
+                        <View className='at-col at-col__offset-1 at-col-1 at-col--auto infoListing info'>
+                            <AtButton
+                                circle
+                                size='small'
+                                type={vehicle.checked_in
+                                    ? 'secondary'
+                                    : 'primary'
+                                }
+                                onClick={vehicle.checked_in
+                                    ? () => handleCheckOut({area: 'security', id: vehicle.id, content: vehicle.plate})
+                                    : () => handleCheckIn({area: 'security', id: vehicle.id, content: vehicle.plate})
+                                }
+                            >
+                                {vehicle.checked_in ? '取消签到' : '签到'}
+                            </AtButton>
+                        </View>
                     </View>
                 ))}
             </AtCard>
