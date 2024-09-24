adaa29dcfc24603faab0874fb600fb1890c30d8b
Brian Lee <48812505+BrianYLee@users.noreply.github.com>
2024-09-24 16:24:17 +0800

    Update AppointmentCard.jsx
    enable rolebased render for appointment card

--- src/components/AppointmentCard/AppointmentCard.jsx
+++ src/components/AppointmentCard/AppointmentCard.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import AdminCard from './AdminCard'
 import SecurityCard from './SecurityCard'
 import JockeyCard from './JockeyCard/JockeyCard'
+import HotelCard from './HotelCard'
+import GolfCard from './GolfCard'
+import { ROLES } from '../../constants/roles'
 import { View } from '@tarojs/components'
-// import { AtCard, AtButton} from 'taro-ui'
 
-// styling
 import './AppointmentCard.scss'
 
-const AppointmentCard = ( { department = null, note = undefined, apmtInfo, handleCheckIn, handleCheckOut, handleEdit=() => null, today } ) => {
+const AppointmentCard = ( { department = null, note = undefined, apmtInfo, handleCheckIn=() => null, handleCheckOut = () => null, handleEdit=() => null, today } ) => {
     return (
         <View className='appointment-card'>
-            <AdminCard apmtInfo={apmtInfo} today={today} handleEdit={handleEdit} />
-            <SecurityCard apmtInfo={apmtInfo} today={today} handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} />
-            <JockeyCard apmtInfo={apmtInfo} today={today} handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} />
+            {department == ROLES.ADMIN && (<AdminCard apmtInfo={apmtInfo} today={today} handleEdit={handleEdit} />)}
+            {department == ROLES.SECURITY && (<SecurityCard apmtInfo={apmtInfo} today={today} handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} />)}
+            {department == ROLES.JOCKEY && (<JockeyCard apmtInfo={apmtInfo} today={today} handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} />)}
+            {department == ROLES.GOLF && (<GolfCard apmtInfo={apmtInfo} today={today} handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} />)}
+            {department == ROLES.HOTEL && (<HotelCard apmtInfo={apmtInfo} today={today} handleCheckIn={handleCheckIn} handleCheckOut={handleCheckOut} />)}
         </View>
     );
 };
